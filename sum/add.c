4c8cc69ff3213382136c5ece1c1f8913e19bd97a
SwetaAgarwal <sweta.agarwal189.com>
2015-10-30 12:49:50 +0530

    Hello.. Added a new file for calculating sum

---
+++ sum/add.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#define A 5
+#define B 7
+int main()
+{
+	int a,b,sum;
+	printf("Enter the numbers : ");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	printf("\n Sum= %d",sum);
+	sum=A+B;
+	printf("\n Sum= %d",sum);
+	return 0;
+}
